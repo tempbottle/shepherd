7b80adc95c98a2e86455e10d4bbf946f19f2e0b9
mccorney <projects.mccorney@gmail.com>
2014-01-04 13:32:17 -0800

    first commit

---
+++ include/shepherd/shepherd.h
@@ -0,0 +1,15 @@
+#ifndef SHEPHERD_INCLUDE_SHEPHERD_H_
+#define SHEPHERD_INCLUDE_SHEPHERD_H_
+
+namespace shepherd {
+
+	class Shepherd{
+		public: 	
+			 
+			Shepherd();
+			~Shepherd();
+
+	};
+
+}
+#endif //SHEPHERD_INCLUDE_SHEPHERD_H_
