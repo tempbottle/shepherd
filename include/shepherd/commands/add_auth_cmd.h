019abdd6f27bba291eda2510399ec32ae68d44a1
McCorney Severin <projects.mccorney@gmail.com>
2014-01-14 20:03:00 -0800

    Update add_auth_cmd.h
    Adding class declaration.

---
+++ include/shepherd/commands/add_auth_cmd.h
@@ -0,0 +1,12 @@
+#ifndef SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ADD_AUTHENTICATION_H_
+#define SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ADD_AUTHENTICATION_H_
+
+namespace shepherd {
+    namespace commands{
+        class AddAuthentication {
+            public:
+                AddAuthentication();
+        };
+    }
+}
+#endif //SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ADD_AUTHENTICATION_H_
