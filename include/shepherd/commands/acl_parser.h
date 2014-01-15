a978244b9630528460d88ead83bd9946accccfa1
McCorney Severin <projects.mccorney@gmail.com>
2014-01-14 20:01:15 -0800

    Update acl_parser.h
    declaring class

---
+++ include/shepherd/commands/acl_parser.h
@@ -0,0 +1,13 @@
+
+#ifndef SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ACLPARSER_H_
+#define SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ACLPARSER_H_
+
+namespace shepherd {
+    namespace commands{
+        class ACLParser {
+            public:
+                ACLParser();
+        };
+    }
+}
+#endif //SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ACLPARSER_H_
