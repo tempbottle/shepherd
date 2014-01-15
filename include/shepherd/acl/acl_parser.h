23b635f338d77efb9206466bf78262c40b270a34
mccorney <projects.mccorney@gmail.com>
2014-01-15 06:02:27 -0800

    adding acl dir

---
+++ include/shepherd/acl/acl_parser.h
@@ -0,0 +1,12 @@
+#ifndef SHEPHERD_INCLUDE_SHEPHERD_ACL_ACLPARSER_H_
+#define SHEPHERD_INCLUDE_SHEPHERD_ACL_ACLPARSER_H_
+
+namespace shepherd {
+    namespace acl{
+        class ACLParser {
+            public:
+                ACLParser();
+        };
+    }
+}
+#endif //SHEPHERD_INCLUDE_SHEPHERD_COMMANDS_ACLPARSER_H_
